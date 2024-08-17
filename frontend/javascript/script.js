b61d9f53e1b40e88d66e6166ab3b7826a04ec7e9
Dapunta <dapuntaxd@gmail.com>
2024-08-18 02:13:44 +0700

    New

---
+++ frontend/javascript/script.js
@@ -0,0 +1,80 @@
+const api = 'https://api.dapuntaratya.com/terabox-api/fetch?url=';
+
+async function fetchURL() {
+
+    const url_element = document.getElementById('input-url');
+    const url = url_element.value.replace(/\s/g, '') === "" ? null : url_element.value;
+
+    if (url) {
+        try {
+            resetter();
+            const api_url = api + encodeURIComponent(url);
+            const response = await fetch(api_url, {
+                method: 'GET',
+                mode: 'cors',
+                headers: {'Content-Type':'application/json', 'Access-Control-Allow-Origin':'*'}
+            });
+            const result = await response.json();
+            if (result.status === 'success') showResult(result.file);
+            else notFound();
+        }
+        catch (error) {errorNotif(error)};
+    }
+
+    url_element.value = "";
+}
+
+function showResult(result) {
+    const plot = document.getElementById('result-container');
+    result.forEach(item => {
+        const child_file = document.createElement('div');
+        child_file.classList.add('file-container');
+        child_file.innerHTML = `
+            <div class="inner-file-container">
+                <div class="thumbnail-container">
+                    <img src="${item.thumbnail}"></img>
+                </div>
+                <div class="file-info-container">
+                    <h3>${item.name}</h3>
+                    <p>${item.size} MB</p>
+                    <div class="file-button-container">
+                        <button class="download-button" type="button" onclick="download('${item.url}')">Server 1</button>
+                        <button class="download-button" type="button" onclick="warning('Server 2 belum tersedia')">Server 2</button>
+                        <button class="download-button" type="button" onclick="warning('Fitur stream belum tersedia')">Stream</button>
+                    </div>
+                </div>
+            </div>`
+        plot.appendChild(child_file);
+    });
+}
+
+function download(url) {
+    window.open(url, '_blank');
+}
+
+function warning(text) {
+    alert(text);
+}
+
+function resetter() {
+    const plot = document.getElementById('result-container');
+    plot.innerHTML = '';
+}
+
+function notFound() {
+    console.log('URL Not Found');
+    const plot = document.getElementById('result-container');
+    plot.innerHTML = `
+        <div class="error-container">
+            <a><i class="fa-solid fa-triangle-exclamation"></i> URL Tidak Ditemukan</a>
+        </div>`
+}
+
+function errorNotif(error) {
+    console.log(error);
+    const plot = document.getElementById('result-container');
+    plot.innerHTML = `
+        <div class="error-container">
+            <a><i class="fa-solid fa-triangle-exclamation"></i> Terjadi Kesalahan</a>
+        </div>`
+}
